fb3ba35bffac51351b1b8917c80ad429106d4997
Suryanshg <suryanshg47@gmail.com>
2019-10-04 18:44:51 -0400

    Part 1 Working!

--- proj4/file.C
+++ proj4/file.C
@@ -25,60 +25,21 @@ long int bytesReg=0, bytesText=0;
 int main(int argc, char *argv[])
 {
 	int fdIn;
-	int  cnt, statResult;
-	bool isText;
-	char buf[BUFSIZE];
 	char fileName[100];
 	struct stat statsInfo;
 
 	if(argc==1){// serial architecture version
 		while(1){
-
 			cin.getline(fileName,100); // get the file's name
-
-
 			if(fileName==NULL || (strcmp(fileName,"\0")==0)){ // if EOF, break
-				cout<<"EOF on "<<fileName<<"\n";
 				break;
 			}
-			statResult=stat(fileName,&statsInfo);
-			if(statResult<0){ // if bad file
-				numBadFiles++;
-			}
-			else if(S_ISDIR(statsInfo.st_mode)){ // if directory
-				numDir++;
-			}
 
-			else if(S_ISREG(statsInfo.st_mode)){ // if regular file
-				numRegFiles++;
-				bytesReg+=statsInfo.st_size;
-				isText=true;
-				if ((fdIn = open(fileName, O_RDONLY)) < 0){ // EOF
-					cout<<"Error opening file:"<<fileName<<"\n";
-				}
-				while ((cnt = read(fdIn, buf, 1)) > 0) { // check for text file
-					if(!(isprint(buf[0])) && !(isspace(buf[0]))){
-						isText=false;
-						break;
-					}
-				}
-				if(isText){
-					numTextFiles++;
-					bytesText+=statsInfo.st_size;
-				}
-
-			}
-			else{ // it's a special file
-				numSpecFiles++;
-			}
-
-			if (fdIn > 0){
-				close(fdIn);
+			else{
+				processFile(fileName,statsInfo,fdIn);
 			}
 		}
-
 		printStats();
-
 	}
 	else if(argc>1){
 		cout<<"MultiThreaded Version\n";
@@ -101,7 +62,6 @@ void processFile(char file[], struct stat statsInfo, int fd){
 	int  cnt, statResult;
 	bool isText;
 	char buf[BUFSIZE];
-
 	statResult=stat(file,&statsInfo);
 	if(statResult<0){ // if bad file
 		numBadFiles++;
@@ -113,6 +73,9 @@ void processFile(char file[], struct stat statsInfo, int fd){
 		numRegFiles++;
 		bytesReg+=statsInfo.st_size;
 		isText=true;
+		if ((fd = open(file, O_RDONLY)) < 0){ // EOF
+			cout<<"Error opening file:"<<file<<"\n";
+		}
 		while ((cnt = read(fd, buf, 1)) > 0) { // check for text file
 			if(!(isprint(buf[0])) && !(isspace(buf[0]))){
 				isText=false;
@@ -123,12 +86,10 @@ void processFile(char file[], struct stat statsInfo, int fd){
 			numTextFiles++;
 			bytesText+=statsInfo.st_size;
 		}
-
 	}
 	else{ // it's a special file
 		numSpecFiles++;
 	}
-
 	if (fd > 0){
 		close(fd);
 	}
